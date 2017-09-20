71362664fa58534e8332972994806c2d49fbf287
Saurabh S <saurabh@appler.xyz>
2017-09-21 00:45:48 +0530

    minor fixes to get latest build onto heroku

--- src/server/common/lib/environment.js
+++ src/server/common/lib/environment.js
@@ -3,21 +3,21 @@ import path from 'path';
 
 switch (process.env.NODE_ENV) {
   case 'prod':
-    Habitat.load(path.resolve(__dirname, '../../config/.env.prod'));
+    Habitat.load(path.resolve(__dirname, '../../../src/config/.env.prod'));
     break;
   case 'test':
-    Habitat.load(path.resolve(__dirname, '../../config/.env.test'));
+    Habitat.load(path.resolve(__dirname, '../../../src/config/.env.test'));
     break;
   case 'development':
     // @review :: sample.env is not supposed to be used anywhere
-    Habitat.load(path.resolve(__dirname, '../../config/.env'));
+    Habitat.load(path.resolve(__dirname, '../../../src/config/.env'));
     break;
   case 'stage':
-    Habitat.load(path.resolve(__dirname, '../../config/.env.stage'));
+    Habitat.load(path.resolve(__dirname, '../../../src/config/.env.stage'));
     break;
   default:
     // @review :: sample.env is not supposed to be used anywhere
-    Habitat.load(path.resolve(__dirname, '../../config/sample.env'));
+    Habitat.load(path.resolve(__dirname, '../../../src/config/sample.env'));
 }
 const env = new Habitat();
 module.exports = env;
